7767070632a3df244fd42435eeac4381ab6e5846
Radek Dobrovolný <radek.dobrovolny@gmail.com>
2024-06-03 22:42:34 +0200

    dark mode

--- src/assets/scripts/xylophone.js
+++ src/assets/scripts/xylophone.js
@@ -15,3 +15,15 @@ document.querySelectorAll('.key').forEach(key => {
         audio.play();
     });
 });
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    if (localStorage.getItem('dark-mode') === 'true') {
+        document.body.classList.add('dark-mode');
+    }
+});
+
+document.getElementById('switch').addEventListener('click', function () {
+    document.body.classList.toggle('dark-mode');
+    localStorage.setItem('dark-mode', document.body.classList.contains('dark-mode'));
+});
