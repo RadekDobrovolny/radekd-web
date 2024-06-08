5935b967065ae4154d535ba24f1547ddbb62a0ee
Radek Dobrovolný <radek.dobrovolny@gmail.com>
2024-06-08 20:22:12 +0200

    fix

--- src/assets/scripts/xylophone.js
+++ src/assets/scripts/xylophone.js
@@ -1,11 +1,11 @@
 const notes = {
-    'C': 'assets/sounds/C.aac',
-    'D': 'assets/sounds/D.aac',
-    'E': 'assets/sounds/E.aac',
-    'F': 'assets/sounds/F.aac',
-    'G': 'assets/sounds/G.aac',
-    'A': 'assets/sounds/A.aac',
-    'B': 'assets/sounds/B.aac'
+    'C': '/assets/sounds/C.aac',
+    'D': '/assets/sounds/D.aac',
+    'E': '/assets/sounds/E.aac',
+    'F': '/assets/sounds/F.aac',
+    'G': '/assets/sounds/G.aac',
+    'A': '/assets/sounds/A.aac',
+    'B': '/assets/sounds/B.aac'
 };
 
 document.querySelectorAll('.key').forEach(key => {
@@ -15,7 +15,6 @@ document.querySelectorAll('.key').forEach(key => {
         audio.play();
     });
 });
-
 
 document.addEventListener('DOMContentLoaded', (event) => {
     if (localStorage.getItem('dark-mode') === 'true') {
