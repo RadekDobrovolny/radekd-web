8d59b54163537d4051df42ad0b67cae5f2d80d7c
Radek Dobrovolný <radek.dobrovolny@gmail.com>
2024-06-08 21:25:42 +0200

    github deploy script

--- src/assets/scripts/xylophone.js
+++ src/assets/scripts/xylophone.js
@@ -1,13 +1,3 @@
-const notes = {
-    'C': '/assets/sounds/C.aac',
-    'D': '/assets/sounds/D.aac',
-    'E': '/assets/sounds/E.aac',
-    'F': '/assets/sounds/F.aac',
-    'G': '/assets/sounds/G.aac',
-    'A': '/assets/sounds/A.aac',
-    'B': '/assets/sounds/B.aac'
-};
-
 document.querySelectorAll('.key').forEach(key => {
     key.addEventListener('click', () => {
         const note = key.getAttribute('data-note');
